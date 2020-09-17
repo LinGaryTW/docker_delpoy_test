cdbd982bdd3362834273c70dbc13f94ea6e0a7ba
Gary Lin <garyqwertyuiop@gmail.com>
2020-09-17 15:13:04 +0800

    fetch fail without token

---
+++ app/javascript/controllers/graphql_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  connect() {
+    console.log(this);
+    this.outputTarget.textContent = 'Hello, graphql!'
+  }
+
+  allLinks () {
+    let result = fetch('http://127.0.0.1:3000/graphql', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ query: '{ allLinks { id } }' }),
+                      })
+                        .then(res => res.json())
+                        .then(res => console.log(res.data));
+    console.log(result);
+  }
+}
